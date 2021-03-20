669c251797e342c3dabcd3aa3011bc3447fa6dd7
DevSamuelV <realalloutmusic@gmail.com>
2021-03-20 10:20:58 -0400

    fixed FRC-Logger

---
+++ src/main/java/frc/robot/FRC-Logger/Csv.java
@@ -0,0 +1,30 @@
+package frc.robot.FRCLogger;
+
+// Csv Extention For FRC Logger
+public class Csv extends Filemanger {
+  private String CompressedRows = "";
+  private Boolean InitalWrite = false;
+
+  public Csv(String filename, String[] rows) {
+    super(filename);
+
+    for (int i = 0; i != rows.length; i++) {
+      String row = rows[i];
+
+      // this is what the expected output is " row one,"
+      row = " " + row + ",";
+
+      // adds the currently formated row to compressed rows
+      CompressedRows += row;
+    }
+  }
+
+  public void LogWithTime(Object data) {
+    if (!this.InitalWrite && this.CompressedRows != null) {
+      this.Write("Time, " + this.CompressedRows + "\n");
+      this.InitalWrite = true;
+    }
+
+    this.Write(java.time.LocalTime.now().toString() + "," + data + "\n");
+  }
+}
